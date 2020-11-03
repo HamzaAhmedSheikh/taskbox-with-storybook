2571a1d5370c28b087854cce05e96cb0f102080a
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-11-04 04:02:53 +0500

    delete some files and folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Task from './components/Task'
+
 
 function App() {
   return (
     <div className="App">
-      <Task />       
+      Hello World
     </div>
   );
 }
