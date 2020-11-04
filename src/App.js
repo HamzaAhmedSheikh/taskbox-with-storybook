d087c5f72a451008e3205c03a9b65f176f817b36
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-11-04 06:07:25 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import { Provider } from 'react-redux';
+import store from './lib/taskbox-redux';
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
       Hello World
-    </div>
+    </Provider>
   );
 }
 
