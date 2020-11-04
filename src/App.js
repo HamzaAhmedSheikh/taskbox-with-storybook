d7e508d5f47ef171afceb3b5c9a6f4a8ddeb5e95
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-11-05 03:51:36 +0500

    add TaskList and test stories

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import './App.css';
 import { Provider } from 'react-redux';
 import store from './lib/taskbox-redux';
+
+import InboxScreen from './components/InboxScreen';
+
+import './index.css';
 
 function App() {
   return (
     <Provider store={store}>
       Hello World
+      <InboxScreen />
     </Provider>
   );
 }
